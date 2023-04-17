7481803a124e9d6149d877e824856ced268dba7c
Raimonds Bortascenoks <r.bortascenoks@gmail.com>
2023-04-17 19:47:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+
+namespace LinqProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = { "VW Golf",
+                                "Opel Astra",
+                                "Audi A4",
+                                "Ford Focus",
+                                "Seat Leon",
+                                "VW Passat",
+                                "VW Polo",
+                                "Mercedes C-Class" };
+
+            for(int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            var listCars = from car in cars select car;
+
+            Console.WriteLine("---------------");
+
+            Console.WriteLine(listCars);
+            ShowELementsValues(listCars);
+
+
+            var listCarsContains = from car in cars where car.Contains("VW") select car;
+
+            Console.WriteLine("---------------");
+            ShowELementsValues(listCarsContains);
+
+        }
+
+        static void ShowELementsValues(IEnumerable<string> list)
+        {
+            
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (string car in list)
+            {
+                stringBuilder.Append(car + "\n");
+            }
+
+            Console.WriteLine(stringBuilder.ToString());
+        }
+    }
+}
